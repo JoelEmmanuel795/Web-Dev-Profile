2be571a19c4c9d340e3d50a7d64bad760e123cf2
Joel Gamonez <j.e.h.gamonez@gmail.com>
2025-02-07 23:00:06 +0100

    Initial commit

---
+++ src/components/LandingSection.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+
+const greeting = "Hello, I am Pete!";
+const bio1 = "A frontend developer";
+const bio2 = "specialised in React";
+
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+     <Avatar src="https://i.pravatar.cc/150?img=7"size='2xl'/>
+    <VStack spacing={2} mt={6}>
+      <Heading as="h3" size="md" noOfLines={1}>
+        {greeting}
+      </Heading>
+      <Heading as="h1" size="4xl" noOfLines={1}>
+        {bio1}
+      </Heading>
+      <Heading as="h1" size="4xl" noOfLines={1}>
+        {bio2}
+      </Heading>
+    </VStack>
+  </FullScreenSection>
+);
+
+export default LandingSection;
