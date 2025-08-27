0698c11a517ef2e30891ecbc5361c76a0a8264b6
Joel Gamonez <j.e.h.gamonez@gmail.com>
2025-08-27 18:18:12 +0200

    Committing new changes to the landing page and other changes for the npm build/deploy process

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
-import profile_image from "../images/profile_image.jpg"
+import profile_image from "../images/profile_image.jpg";
 
 const greeting = "Hello, I am Joel!";
 const bio1 = "A fullstack developer specialising in";
-const bio2 = "Python, C, JavaScript, HTML, CSS, and React";
+const bio2 = "Python, C, JavaScript, HTML, CSS, React, Redux,";
+const bio3 = "Docker, GitLab CI/CD, Django, and more!";
 
 const LandingSection = () => (
   <FullScreenSection
@@ -14,16 +15,19 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-    <Avatar src={profile_image} size={{ base: 'xl', sm: '2xl' }} />
+    <Avatar src={profile_image} size={{ base: "xl", sm: "2xl" }} />
     <VStack spacing={2} mt={6}>
-      <Heading as="h3" size={{ base: 'sm', sm: 'md' }} noOfLines={1}>
+      <Heading as="h3" size={{ base: "sm", sm: "md" }} noOfLines={1}>
         {greeting}
       </Heading>
-      <Heading as="h1" size={{ base: 'lg', sm: 'xl' }}>
+      <Heading as="h1" size={{ base: "lg", sm: "lg" }}>
         {bio1}
       </Heading>
-      <Heading as="h1" size={{ base: 'lg', sm: 'xl' }}>
+      <Heading as="h1" size={{ base: "lg", sm: "lg" }}>
         {bio2}
+      </Heading>
+      <Heading as="h1" size={{ base: "lg", sm: "lg" }}>
+        {bio3}
       </Heading>
     </VStack>
   </FullScreenSection>
